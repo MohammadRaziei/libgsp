4321bb79c6940f6c94780b7e2c573b63f29b8627
Mohammad Raziei <mohammadraziei1375@gmail.com>
2025-07-12 16:26:09 +0000

    implement SVG rendering functionality with mustache template and add graph.svg.mustache

--- includes/render.h
+++ includes/render.h
@@ -1,20 +1,7 @@
-//
-// Created by mohammad on 5/20/23.
-//
-#include <iostream>
-#include <memory> 
-
-#include <aixlog.hpp>
+#include <fstream>
+#include <sstream>
+#include <cmath>
 #include <mustache.hpp>
-
-#include "io/file.h"
-
-#include "common.h"
-
-#undef LOGGER
-#define LOGGER(level) LOG(level) << TAG("main") << " "
-
-
 
 std::string render_svg(
     const std::vector<std::tuple<int, int>> &edges,
@@ -38,8 +25,6 @@ std::string render_svg(
         max_y = std::max(max_y, std::max(y, y_sig) + 20);
     }
 
-    char buf[32];
-
     kainjow::mustache::data ctx;
     ctx.set("title", "Network");
     ctx.set("width", std::to_string((int)(max_x - min_x)));
@@ -51,8 +36,9 @@ std::string render_svg(
     auto &edges_data = ctx["edges"];
     auto &nodes_data = ctx["nodes"];
 
+    // اضافه کردن یال‌ها
     for (auto &[src, tgt] : edges) {
-        kainjow::mustache::data e(kainjow::mustache::data::type::object);
+        auto &e = edges_data.push_back(kainjow::mustache::data::type::object);
         auto [x1, y1] = coords[src];
         auto [x2, y2] = coords[tgt];
         e.set("x1", std::to_string(x1 * scale - min_x));
@@ -61,11 +47,10 @@ std::string render_svg(
         e.set("y2", std::to_string(-y2 * scale + max_y));
         e.set("src", std::to_string(src));
         e.set("tgt", std::to_string(tgt));
-        edges_data.push_back(e);
     }
 
     for (size_t i = 0; i < coords.size(); ++i) {
-        kainjow::mustache::data n(kainjow::mustache::data::type::object);
+        auto &n = nodes_data.push_back(kainjow::mustache::data::type::object);
         double x = coords[i].first * scale - min_x;
         double y = -coords[i].second * scale + max_y;
         double signal = signals[i];
@@ -73,8 +58,7 @@ std::string render_svg(
         n.set("x", std::to_string(x));
         n.set("y", std::to_string(y));
         n.set("label", std::to_string(i));
-        std::sprintf(buf, "%g", signal);
-        n.set("signal", buf);
+        n.set("signal", std::to_string(signal));
 
         if (signal != 0) {
             n.set("has_signal", "true");
@@ -83,45 +67,12 @@ std::string render_svg(
             n.set("y2", std::to_string(y + value));
             n.set("text_y", std::to_string(y + value + (value < 0 ? -font_size : font_size)));
         }
-        nodes_data.push_back(n);
     }
 
-    const std::string template_file = readFile("includes/io/templates/svg/graph.svg.mustache");
-
-    kainjow::mustache::mustache tmpl(template_file);
+    std::ifstream template_file("graph.svg.mustache");
+    std::stringstream buffer;
+    buffer << template_file.rdbuf();
+    kainjow::mustache::mustache tmpl(buffer.str());
 
     return tmpl.render(ctx);
-}
-
-
-int main(int argc, char** argv){
-    AixLog::Log::init(
-        {
-            std::make_shared<AixLog::SinkCout>(AixLog::Severity::trace, "cout: %Y-%m-%d %H-%M-%S.#ms [#severity] (#tag) #message"),
-
-        }
-    );
-
-    LOGGER(INFO) << "Hello, world!";
-
-
-
-    printf("\ngood bye :)\n");
-
-
-
-
-    std::vector<std::tuple<int, int>> edges = {{0,1},{0,2},{1,2},{2,3}};
-    std::vector<std::pair<double,double>> coords = {{0,0}, {2,0}, {1,-1}, {3,-1}};
-    std::vector<double> signals = {-0.04, 0.31, 0.06, 0.39};
-
-    std::string svg = render_svg(edges, coords, signals);
-
-    std::cout << "SVG Output:\n" << svg << std::endl;
-
-
-    std::ofstream out("output.svg");
-    out << svg;
-		
-    return 0;
 }
