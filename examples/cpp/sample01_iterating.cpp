dd5312875fd06c2a5794a3f8b9ed06683d9a2197
MohammadRaziei <mohammadraziei1375@gmail.com>
2025-08-23 10:33:54 +0330

    add countNumNodes and countNumEdges functions

--- examples/cpp/sample01_iterating.cpp
+++ examples/cpp/sample01_iterating.cpp
@@ -4,6 +4,28 @@
 
 #include "libgsp/graph/graph.h"
 
+uint32_t countNumNodes(gsp::BaseGraph& graph) {
+    uint32_t count_nodes = 0;
+    graph.nodeIter();
+    while (auto node = graph.nodeNext()) {
+        printf("Node %i: %s @ (%.5g,%.5g,%.5g)\n", node->id, node->name.c_str(), node->coord.x, node->coord.y, node->coord.z);
+        ++count_nodes;
+    }
+    printf("There is %i nodes\n", count_nodes);
+    return count_nodes;
+}
+
+uint32_t countNumEdges(gsp::BaseGraph& graph) {
+    uint32_t num_edges = 0;
+    graph.edgeIter();
+    while (auto edge = graph.edgeNext()) {
+        printf("Edge: from[%i] to[%i] weight[%.5g]\n", edge->source, edge->target, edge->weight);
+        ++num_edges;
+    }
+    printf("There is %i edges\n", num_edges);
+    return num_edges;
+}
+
 
 int main() {
     std::vector<gsp::Edge>  edges      = {{0, 0},{0,1, 3},{0,2},{1,2},{2,3}};
@@ -15,55 +37,24 @@ int main() {
     // Build graph
     gsp::DenseGraph graph(num_nodes);
 
-    uint32_t count_nodes, num_edges;
-
-    count_nodes = 0;
-    graph.nodeIter();
-    while (auto node = graph.nodeNext()) {
-        printf("Node %i: %s @ (%.5g,%.5g,%.5g)\n", node->id, node->name.c_str(), node->coord.x, node->coord.y, node->coord.z);
-        ++count_nodes;
-    }
-    printf("There is %i nodes\n", count_nodes);
+    countNumNodes(graph);
 
     graph.setCoords(coords_vec);
 
-    count_nodes = 0;
-    graph.nodeIter();
-    while (auto node = graph.nodeNext()) {
-        printf("Node %i: %s @ (%.5g,%.5g,%.5g)\n", node->id, node->name.c_str(), node->coord.x, node->coord.y, node->coord.z);
-        ++count_nodes;
-    }
-    printf("There is %i nodes\n", count_nodes);
+    countNumNodes(graph);
 
     graph.setNames({"A", "B", "C", "D"});
 
-    count_nodes = 0;
-    graph.nodeIter();
-    while (auto node = graph.nodeNext()) {
-        printf("Node %i: %s @ (%.5g,%.5g,%.5g)\n", node->id, node->name.c_str(), node->coord.x, node->coord.y, node->coord.z);
-        ++count_nodes;
-    }
-    printf("There is %i nodes\n", count_nodes);
+    countNumNodes(graph);
 
 
-    num_edges = 0;
-    graph.edgeIter();
-    while (auto edge = graph.edgeNext()) {
-        printf("Edge: from[%i] to[%i] weight[%.5g]\n", edge->source, edge->target, edge->weight);
-        ++num_edges;
-    }
-    printf("There is %i edges\n", num_edges);
+    countNumEdges(graph);
+
 
     graph.setEdges(edges);           // fills graph.weights
 
 
-    num_edges = 0;
-    graph.edgeIter();
-    while (auto edge = graph.edgeNext()) {
-        printf("Edge: from[%i] to[%i] weight[%.5g]\n", edge->source, edge->target, edge->weight);
-        ++num_edges;
-    }
-    printf("There is %i edges\n", num_edges);
+    countNumEdges(graph);
 
     return 0;
 }
