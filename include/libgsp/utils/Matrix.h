e8191bf967853307a1b853292ae1a4c47846148b
mohammadraziei <mohammadraziei1375@gmail.com>
2025-10-07 19:28:50 +0330

    fix assertion

--- include/libgsp/utils/Matrix.h
+++ include/libgsp/utils/Matrix.h
@@ -13,45 +13,16 @@
 #include "libgsp/utils/Types.h"
 
 namespace gsp::matrix {
-// ======== Declarations ========
-
-// allocate
-template<typename Matrix>
-void allocate(Matrix& matrix, uint32_t rows, uint32_t cols);
-
-// fillZero
-template<typename Matrix>
-void fillZero(Matrix& matrix);
-
-// free
-template<typename Matrix>
-void free(Matrix& matrix);
-
-// setElement
-template<typename Matrix>
-void setElement(Matrix& matrix,
-                uint32_t row, uint32_t col,
-                gsp::types::elem_t<Matrix> el);
-
-// getElement
-template<typename Matrix>
-gsp::types::elem_t<Matrix>
-getElement(Matrix& matrix, uint32_t row, uint32_t col);
-
-} // namespace gsp::matrix
-
-// ======== Implementations ========
-
 // ---------- allocate ----------
 template<typename Scalar, int Options, typename Index>
-void gsp::matrix::allocate(Eigen::SparseMatrix<Scalar, Options, Index>& m,
+void allocate(Eigen::SparseMatrix<Scalar, Options, Index>& m,
               uint32_t rows, uint32_t cols) {
     m.resize(static_cast<Eigen::Index>(rows),
              static_cast<Eigen::Index>(cols));
 }
 
 template<typename Scalar, int Rows, int Cols, int Options, int MR, int MC>
-void gsp::matrix::allocate(Eigen::Matrix<Scalar, Rows, Cols, Options, MR, MC>& m,
+void allocate(Eigen::Matrix<Scalar, Rows, Cols, Options, MR, MC>& m,
               uint32_t rows, uint32_t cols) {
     m.resize(static_cast<Eigen::Index>(rows),
              static_cast<Eigen::Index>(cols));
@@ -59,29 +30,29 @@ void gsp::matrix::allocate(Eigen::Matrix<Scalar, Rows, Cols, Options, MR, MC>& m
 
 // ---------- fillZero ----------
 template<typename Scalar, int Rows, int Cols, int Options, int MR, int MC>
-void gsp::matrix::fillZero(Eigen::Matrix<Scalar, Rows, Cols, Options, MR, MC>& matrix) {
+void fillZero(Eigen::Matrix<Scalar, Rows, Cols, Options, MR, MC>& matrix) {
     matrix.setZero();
 }
 
 template<typename Scalar, int Options, typename Index>
-void gsp::matrix::fillZero(Eigen::SparseMatrix<Scalar, Options, Index>& matrix) {
+void fillZero(Eigen::SparseMatrix<Scalar, Options, Index>& matrix) {
     matrix.setZero();
 }
 
 // ---------- free ----------
 template<typename Scalar, int Options, typename Index>
-void gsp::matrix::free(Eigen::SparseMatrix<Scalar, Options, Index>& m) {
+void free(Eigen::SparseMatrix<Scalar, Options, Index>& m) {
     m.resize(0, 0);
 }
 
 template<typename Scalar, int Rows, int Cols, int Options, int MR, int MC>
-void gsp::matrix::free(Eigen::Matrix<Scalar, Rows, Cols, Options, MR, MC>& m) {
+void free(Eigen::Matrix<Scalar, Rows, Cols, Options, MR, MC>& m) {
     m.resize(0, 0);
 }
 
 // ---------- setElement ----------
 template<typename Scalar, int Options, typename Index>
-void gsp::matrix::setElement(Eigen::SparseMatrix<Scalar, Options, Index>& m,
+void setElement(Eigen::SparseMatrix<Scalar, Options, Index>& m,
                 uint32_t r, uint32_t c, Scalar el) {
     if (m.isCompressed()) m.uncompress();
     m.coeffRef(static_cast<Eigen::Index>(r),
@@ -89,7 +60,7 @@ void gsp::matrix::setElement(Eigen::SparseMatrix<Scalar, Options, Index>& m,
 }
 
 template<typename Scalar, int Rows, int Cols, int Options, int MR, int MC>
-void gsp::matrix::setElement(Eigen::Matrix<Scalar, Rows, Cols, Options, MR, MC>& m,
+void setElement(Eigen::Matrix<Scalar, Rows, Cols, Options, MR, MC>& m,
                 uint32_t r, uint32_t c, Scalar el) {
     m(static_cast<Eigen::Index>(r),
       static_cast<Eigen::Index>(c)) = el;
@@ -97,18 +68,19 @@ void gsp::matrix::setElement(Eigen::Matrix<Scalar, Rows, Cols, Options, MR, MC>&
 
 // ---------- getElement ----------
 template<typename Scalar, int Options, typename Index>
-Scalar gsp::matrix::getElement(Eigen::SparseMatrix<Scalar, Options, Index>& m,
+Scalar getElement(Eigen::SparseMatrix<Scalar, Options, Index>& m,
                   uint32_t r, uint32_t c) {
     return m.coeff(static_cast<Eigen::Index>(r),
                    static_cast<Eigen::Index>(c));
 }
 
 template<typename Scalar, int Rows, int Cols, int Options, int MR, int MC>
-Scalar gsp::matrix::getElement(Eigen::Matrix<Scalar, Rows, Cols, Options, MR, MC>& m,
+Scalar getElement(Eigen::Matrix<Scalar, Rows, Cols, Options, MR, MC>& m,
                   uint32_t r, uint32_t c) {
     return m(static_cast<Eigen::Index>(r),
              static_cast<Eigen::Index>(c));
 }
+} // namespace gsp::matrix
 
 
 #endif // LIBGSP_MATRIX_H
